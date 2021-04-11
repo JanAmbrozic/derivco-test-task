cddf66de19752228fc04317e9bb10b7c794e5f5b
Jan Ambrozic <jan3594@gmail.com>
2021-04-11 22:19:40 +0200

    fix(resultsEval): add combo matches and cleanup

--- src/components/slot/resultsEval.js
+++ src/components/slot/resultsEval.js
@@ -2,121 +2,123 @@
  * Evaluation of results and matching with winlines defined in `data/slot.js`
  */
 export default class ResultsEval {
-  /**
-   * Starts the entity which adds a sprite. Very basic for now.
-   */
-  start(entity) {
-    this.entity = entity;
-    this.container = PIXI.Sprite.fromFrame(entity.attributes.textureId);
-    entity.container.addChild(this.container);
-  }
 
 
-  setTexture(textureId) {
-    if(!PIXI.utils.TextureCache[textureId]) {
-      textureId = PIXI.Texture.EMPTY;
-    }
-    this.container.texture = PIXI.Sprite.fromFrame(textureId);
-  }
-
-
-  /**
-   * Pay-table must indicate winning combinations and payouts as: 3 CHERRY symbols on top line 2000
-3 CHERRY symbols on center line 1000
-3 CHERRY symbols on bottom line 4000
- 
-3 7 symbols on any line 150
-Any combination of CHERRY and 7 on any line 75 3 3xBAR symbols on any line 50
-3 2xBAR symbols on any line 20
-3 BAR symbols on any line 10
-Combination of any BAR symbols on any line 5
-
-
-        winningLineNumber: 0,
-        positions: [0, 0, 0],
-        sym: ["CHERRY"],
-        winAmount: 2000,
-   * 
-   */
+/**
+ * Checks for winlines. This code could be improved but since this would be sent back from a 
+ * server in a realistic game it's just a rough mock.
+ */
   checkWinningLines(reels, winlineData) {
 
     let winlines = [];
     let totalWin = 0;
 
     //check if all numbers of visible symbols are equal, if not we do not have a win
-    if(reels[0].getVisibleSymbols().length === reels[1].getVisibleSymbols().length 
-    && reels[0].getVisibleSymbols().length === reels[2].getVisibleSymbols().length ) {
-        //if that is the case compare lines to paytable data
+    if (reels[0].getVisibleSymbols().length === reels[1].getVisibleSymbols().length
+      && reels[0].getVisibleSymbols().length === reels[2].getVisibleSymbols().length) {
+      //if that is the case compare lines to paytable data
+      for (const winline of winlineData) {
+        let newWinline;
 
-
-
-        for (const winline of winlineData) {
-
-            // we know we have a center win line
-            if(reels[0].getVisibleSymbols().length === 1) {
-                // we filter out only center lines
-                if(winline.type === 'center') {
-                    let newWinline = this.checkFor3SymbolsMatch(reels, winline, 0);
-                    if(newWinline){
-                        winlines.push(newWinline);
-                        totalWin += newWinline.winAmount;
-                    }
-                }
-
-                // if(reels[0].getVisibleSymbols()[0] === reels[1].getVisibleSymbols()[0] &&
-                //     reels[0].getVisibleSymbols()[0] === reels[2].getVisibleSymbols()[0] && 
-                //     winline.sym[0] === reels[0].getVisibleSymbols()[0]) 
-                // {
-                //         let foundWinline = {
-                //             winline: winline,
-                //             type: 'center',
-                //             winAmount: winline.winAmount
-                //         }
-                //         winlines.push(foundWinline);
-                // }
-
-            } else { // we know we a top and bottom winline
-                let newWinline;
-                if(winline.type === 'top') {
-                    newWinline = this.checkFor3SymbolsMatch(reels, winline, 0);
-                } else if(winline.type === 'bottom'){
-                    newWinline = this.checkFor3SymbolsMatch(reels, winline, 1);
-                }
-
-                if(newWinline){
-                    totalWin += newWinline.winAmount;
-                    winlines.push(newWinline);
-                }
-            }            
+        // we know we have a center win line
+        if (reels[0].getVisibleSymbols().length === 1) {
+          // we filter out only center lines
+          if (winline.type === 'center') {
+            newWinline = this.checkMatches(reels, winline, 0);
+          }
+        } else { // we know we a top and bottom winline
+          if (winline.type === 'top') {
+            newWinline = this.checkMatches(reels, winline, 0);
+          } else if (winline.type === 'bottom') {
+            newWinline = this.checkMatches(reels, winline, 1);
+          }
         }
-    } 
+
+        if (newWinline) {
+          totalWin += newWinline.winAmount;
+          winlines.push(newWinline);
+        }
+      }
+    }
 
     return {
       winlines,
       totalWin
-      };
+    };
+  }
+
+  checkMatches(reels, winline, index) {
+    return this.checkFor3SymbolsMatch(reels, winline, index) || this.checkForCherry7Wins(reels, winline, index) || this.checkForBarCombo(reels, winline, index);
   }
 
   checkFor3SymbolsMatch(reels, winline, index) {
     //check if all 3 symbols match and if winline sym input only has one winning symbol (no combinations are possible)
-    if( reels[0].getVisibleSymbols()[index] === reels[1].getVisibleSymbols()[index] &&
-        reels[0].getVisibleSymbols()[index] === reels[2].getVisibleSymbols()[index] && 
-        winline.sym.length === 1 && winline.sym[0] === reels[0].getVisibleSymbols()[index]) {
-        let foundWinline = {
-            id: winline.winlineId,
-            sym: winline.sym,
-            type: winline.type,
-            winAmount: winline.winAmount
-        }
-        return foundWinline;
+    if (reels[0].getVisibleSymbols()[index] === reels[1].getVisibleSymbols()[index] &&
+      reels[0].getVisibleSymbols()[index] === reels[2].getVisibleSymbols()[index] &&
+      winline.sym.length === 1 && winline.sym[0] === reels[0].getVisibleSymbols()[index]) {
+      let foundWinline = {
+        id: winline.winlineId,
+        sym: winline.sym,
+        type: winline.type,
+        winAmount: winline.winAmount
+      };
+      return foundWinline;
     }
     return null;
   }
 
   //Any combination of CHERRY and 7 on any line 75
-  checkForCherry7Wins () {
-    //todo add combo evaluations
+  checkForCherry7Wins(reels, winline, index) {
+
+    let match = true;
+
+    for (const reel of reels) {
+      if (reel.getVisibleSymbols()[index] !== "CHERRY" && reel.getVisibleSymbols()[index] !== "7") {
+        match = false;
+      }
+    }
+
+    if (match && winline.sym.length === 2 && winline.sym[0] === "7" && winline.sym[1] === "CHERRY") {
+      let foundWinline = {
+        id: winline.winlineId,
+        sym: winline.sym,
+        type: winline.type,
+        winAmount: winline.winAmount
+      };
+      return foundWinline;
+    }
+
+    return null;
+
   }
 
-  
+  checkForBarCombo(reels, winline, index) {
+
+    let match = true;
+
+    for (const reel of reels) {
+      if (reel.getVisibleSymbols()[index] !== "BAR" && reel.getVisibleSymbols()[index] !== "2xBAR" && reel.getVisibleSymbols()[index] !== "3xBAR") {
+        match = false;
+      }
+    }
+    //check if all bars are the same, then we have a higher winning combination
+    if (reels[0].getVisibleSymbols()[index] === reels[1].getVisibleSymbols()[index] &&
+      reels[0].getVisibleSymbols()[index] === reels[2].getVisibleSymbols()[index]) {
+      return null;
+    }
+
+
+    if (match && winline.sym.length === 3) {
+      let foundWinline = {
+        id: winline.winlineId,
+        sym: winline.sym,
+        type: winline.type,
+        winAmount: winline.winAmount
+      };
+      return foundWinline;
+    }
+
+    return null;
+
+  }
 }
